2caf74f60ab4047d33401a3e1591bc1779fd82c7
Mohammad Faruque <323642574+farukparsa@users.noreply.github.com>
2026-09-06 17:36:00 +0600

    Replace Laravel shell with APONWORKS layout

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import AppContent from '@/components/AppContent.vue';
 import AppShell from '@/components/AppShell.vue';
-import AppSidebar from '@/components/AppSidebar.vue';
-import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
 import { Toaster } from '@/components/ui/sonner';
 import type { BreadcrumbItem } from '@/types';
 
@@ -16,12 +14,11 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-    <AppShell variant="sidebar">
-        <AppSidebar />
-        <AppContent variant="sidebar" class="min-w-0 overflow-x-clip">
-            <AppSidebarHeader :breadcrumbs="breadcrumbs" />
+    <AppShell>
+        <AppContent class="min-w-0 overflow-x-clip">
             <slot />
         </AppContent>
+
         <Toaster />
     </AppShell>
 </template>
